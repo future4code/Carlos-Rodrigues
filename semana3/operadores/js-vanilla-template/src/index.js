a9bd505573a6824efaa3676960fbb573cb6d0011
Carlos-hr <carlos.henrique17.chr@gmail.com>
2021-04-20 14:21:40 -0300

    fiz até o 2 escrito

---
+++ semana3/operadores/js-vanilla-template/src/index.js
@@ -0,0 +1,45 @@
+//Exercícios de interpretação de código
+
+//1)
+//a. false
+//b. false
+//c. true
+//e. boolean
+
+//2)
+//a. undefined
+//b. null
+//c. 11
+//d. 3
+//e. [3, 19, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+//f. 9
+
+
+// Exercícios de escrita
+
+//1)
+
+// let idadeUser = Number(prompt("Qual sua idade?"))
+// let idadeAmigo = Number(prompt("Qual idade do seu melhor amigo(a)"))
+
+// let comparador = idadeUser > idadeAmigo
+// console.log(`Sua idade é maior do que a do seu melhor amigo? ${comparador}`)
+
+// let difencaIdade = idadeUser - idadeAmigo
+
+// if (difencaIdade < 0) {
+//     difencaIdade *= -1
+// }
+
+// console.log(`Diferença de idade: ${difencaIdade}`)
+
+
+//2)
+
+// let input = Number(prompt ("Digite um número par: ") )
+
+// let resto = input % 2
+// console.log(`Resto divisão por 2 = ${resto}`)
+
+// O resto da divisão de um número par por 2 vai ser sempre 0, caso seja colocado um número ímpar
+//inteiro, o resto será 1.
