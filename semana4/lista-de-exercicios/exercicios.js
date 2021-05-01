2f71c17339fc28fadb5d93cf341bc55196f3694c
Carlos-hr <carlos.henrique17.chr@gmail.com>
2021-05-01 15:31:35 -0300

    exercicio 11

--- semana4/lista-de-exercicios/exercicios.js
+++ semana4/lista-de-exercicios/exercicios.js
@@ -161,7 +161,17 @@ function segundoMaiorEMenor(array) {
 //Exercício 11
 
 function ordenaArray(array) {
-   
+
+   for (let i = 0; i < array.length; i++) {
+      for (let j = 0; j < array.length; j++) {
+         if (array[j] > array[j+1]) {
+            let ordena = array[j]
+            array[j] = array[j+1]
+            array[j+1] = ordena 
+         }   
+      }    
+   }
+   return array
 }
 
 // Exercício 12
