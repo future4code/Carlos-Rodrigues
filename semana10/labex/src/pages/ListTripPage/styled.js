0c8818ddc26f82e10efe9ddd103bfbfdc88c9fad
Carlos-hr <carlos.henrique17.chr@gmail.com>
2021-06-14 10:33:35 -0300

    Correção no estilo

--- semana10/labex/src/pages/ListTripPage/styled.js
+++ semana10/labex/src/pages/ListTripPage/styled.js
@@ -4,6 +4,7 @@ import ceu from '../../img/ceu.jpg';
 export const MainContainer = styled.div `
     background-image: url(${ceu});
     background-size: cover;
+    height: 100vh;
 `
 
 export const TopContainer = styled.div `
