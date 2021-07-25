3770e6073e65ee1ff381a1a49e9c8a8445f3c3f2
Carlos-hr <carlos.henrique17.chr@gmail.com>
2021-07-25 17:33:13 -0300

    Endpoint createUser

---
+++ semana16/To-Do-List/src/endpoints/createUser.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from "express";
+import insertUser from "../data/insertUser";
+
+
+export default async function createUser (
+    req: Request,
+    res: Response
+) {
+    try {
+        const {name, nickname, email} = req.body
+        if ( !name || !nickname || !email) {
+            res.status(400).send("Preencha todos os campos")
+        }
+
+        const id: string = Date.now() + Math.random().toString()
+        
+        await insertUser(id, name, nickname, email)
+    } catch (error) {
+        res.status(400).send ({
+            message: error.message || error.sqlMessage
+        })
+    }
+}
