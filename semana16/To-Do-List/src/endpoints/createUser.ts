402e92637816b57384affef8ad0b53459ac55346
Carlos-hr <carlos.henrique17.chr@gmail.com>
2021-07-26 07:48:41 -0300

    Bug endpoint

--- semana16/To-Do-List/src/endpoints/createUser.ts
+++ semana16/To-Do-List/src/endpoints/createUser.ts
@@ -10,14 +10,18 @@ export default async function createUser (
         const {name, nickname, email} = req.body
         if ( !name || !nickname || !email) {
             res.status(400).send("Preencha todos os campos")
+            console.log("erro if")
         }
 
         const id: string = Date.now() + Math.random().toString()
         
         await insertUser(id, name, nickname, email)
+        res.status(200).send("Usuário criado.")
+
     } catch (error) {
         res.status(400).send ({
             message: error.message || error.sqlMessage
         })
+        console.log("erro")
     }
 }
